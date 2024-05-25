95ef7769325712748a34776d1a30a6ab964943d2
Ivaylo Zahariev <izahariev95@gmail.com>
2024-05-25 15:50:23 +0300

    [T] Added ExchangeService unit tests
    Added ExchangeServiceTest class and a test which validates the getRate
    method behaviour when given valid currencies and amount.

---
+++ src/test/java/com/example/currencyconverter/services/ExchangeServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.currencyconverter.services;
+
+import com.example.currencyconverter.clients.CurrencyApiClient;
+import com.example.currencyconverter.models.Currency;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ExchangeServiceTest {
+
+    @Mock
+    CurrencyApiClient mockCurrencyApiClient;
+
+    @Test
+    public void givenValidCurrencies_whenRetrievingRate_thenReturnExchangeRate() {
+        when(
+                mockCurrencyApiClient.convert(
+                        Mockito.any(Currency.class),
+                        Mockito.any(Currency.class),
+                        Mockito.anyDouble()
+                )
+        ).thenReturn(1.95);
+
+        ExchangeService exchangeService = new ExchangeService(mockCurrencyApiClient);
+        Assertions.assertEquals(
+                1.95,
+                exchangeService.getRate(Currency.EUR, Currency.BGN, 1),
+                "Incorrect exchange rate");
+    }
+}
