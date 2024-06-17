d495699cda03af72fcce35cca925208c68bb7c16
Ivaylo Zahariev <izahariev95@gmail.com>
2024-06-17 20:57:43 +0300

    [T] Added unit tests for the ExchangeService::convert method
    Added a test which validates the method returns correct data when given
    valid currencies and amount.
    
    Added a test which validates the method throw exception when given
    invalid amount

--- src/test/java/com/example/currencyconverter/services/ExchangeServiceTest.java
+++ src/test/java/com/example/currencyconverter/services/ExchangeServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.currencyconverter.services;
 
 import com.example.currencyconverter.clients.CurrencyApiClient;
+import com.example.currencyconverter.entities.ConversionEntity;
+import com.example.currencyconverter.models.ConversionResult;
 import com.example.currencyconverter.models.Currency;
 import com.example.currencyconverter.repositories.ConversionRepository;
 import org.junit.jupiter.api.Assertions;
@@ -11,6 +13,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 import static org.mockito.Mockito.when;
 
@@ -20,7 +23,7 @@ public class ExchangeServiceTest {
     @Mock
     private CurrencyApiClient mockCurrencyApiClient;
     @Mock
-    private ConversionRepository conversionRepository;
+    private ConversionRepository mockConversionRepository;
 
     @Test
     public void givenValidCurrencies_whenRetrievingRate_thenReturnExchangeRate() {
@@ -32,7 +35,7 @@ public class ExchangeServiceTest {
                 )
         ).thenReturn(BigDecimal.valueOf(1.95));
 
-        ExchangeService exchangeService = new ExchangeService(mockCurrencyApiClient, conversionRepository);
+        ExchangeService exchangeService = new ExchangeService(mockCurrencyApiClient, mockConversionRepository);
         Assertions.assertEquals(
                 BigDecimal.valueOf(1.95),
                 exchangeService.getRate(Currency.EUR, Currency.BGN, BigDecimal.ONE),
@@ -41,11 +44,60 @@ public class ExchangeServiceTest {
 
     @Test
     public void givenInvalidAmount_whenRetrievingRate_thenThrowException() {
-        ExchangeService exchangeService = new ExchangeService(new CurrencyApiClient(), conversionRepository);
+        ExchangeService exchangeService = new ExchangeService(new CurrencyApiClient(), mockConversionRepository);
         Assertions.assertThrows(
                 RuntimeException.class,
                 () -> exchangeService.getRate(Currency.EUR, Currency.BGN, BigDecimal.ZERO),
                 "Invalid amount should not be accepted"
         );
+    }
+
+    @Test
+    public void givenValidData_whenConvertingCurrencies_thenReturnConvertedResult() {
+        when(
+                mockCurrencyApiClient.convert(
+                        Mockito.any(Currency.class),
+                        Mockito.any(Currency.class),
+                        Mockito.any()
+                )
+        ).thenReturn(BigDecimal.valueOf(195));
+
+        long timeMillis = System.currentTimeMillis();
+        ConversionEntity conversionEntity = new ConversionEntity();
+        conversionEntity.setId(Constants.UUID_CONSTANT);
+        conversionEntity.setFromCurrency(Currency.EUR);
+        conversionEntity.setToCurrency(Currency.BGN);
+        conversionEntity.setSourceAmount(BigDecimal.valueOf(100));
+        conversionEntity.setConvertedAmount(BigDecimal.valueOf(195));
+        conversionEntity.setEpochMilliseconds(timeMillis);
+        when(mockConversionRepository.save(Mockito.any()))
+                .thenReturn(conversionEntity);
+
+        ExchangeService exchangeService = new ExchangeService(mockCurrencyApiClient, mockConversionRepository);
+        ConversionResult conversionResult = exchangeService.convert(Currency.EUR, Currency.BGN, BigDecimal.valueOf(100));
+        Assertions.assertEquals(
+                Constants.UUID_CONSTANT,
+                conversionResult.id(),
+                "Incorrect conversion ID"
+        );
+        Assertions.assertEquals(
+                BigDecimal.valueOf(195),
+                conversionResult.value(),
+                "Incorrect conversion result"
+        );
+    }
+
+    @Test
+    public void givenInvalidAmount_whenConvertingCurrencies_thenThrowException() {
+        ExchangeService exchangeService = new ExchangeService(new CurrencyApiClient(), mockConversionRepository);
+        Assertions.assertThrows(
+                RuntimeException.class,
+                () -> exchangeService.convert(Currency.EUR, Currency.BGN, BigDecimal.ZERO),
+                "Invalid amount should not be accepted"
+        );
+    }
+
+    private static class Constants {
+        public static UUID UUID_CONSTANT = UUID.fromString("5c8449a5-2633-48cf-afc1-fb364c8c0fd5");
     }
 }
